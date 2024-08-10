2487cfb53d31b1b1ac0be9ab83592204584f9304
imbirdperson <birdperson.me@gmail.com>
2024-08-10 22:54:54 +0930

    'update

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+const Header = () => {
+    return (
+        <>
+        <div className="header-wrapper">
+            <div className="logo">
+                DEEPAK RAJAN ~ 2024
+            </div>
+            <div className="menu-wrapper">
+                <div className="menu">
+                    ART
+                </div>
+                <div className="menu">
+                    WORKS
+                </div>
+                <div className="menu">
+                    BLOGS
+                </div>
+                <div className="menu">
+                    PROFILE
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Header;
